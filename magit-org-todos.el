c95d7a42f9c9b351fb3fa72bb87f1ab99ef96df5
John Hamelink <me@johnhame.link>
2023-04-12 13:55:01 +0100

    Extract function for handling org elements, support verbatim element

--- magit-org-todos.el
+++ magit-org-todos.el
@@ -45,6 +45,16 @@
     (define-key m [remap magit-visit-thing] 'magit-org-todos--magit-visit-org-todo)
     m))
 
+(defun magit-org-todos--handle-element (el)
+  "Handle org elements when rendering the TODO list in magit."
+  (cond ((not (listp el)) el)
+        ;; TODO: Propertize this instead to provide clickable links.
+        ((eq (car el) 'link) (concat (car (last el)) " "))
+        ((eq (car el) 'verbatim)
+         (let ((value (plist-get (cadr el) :value)))
+           (concat (propertize value 'face 'org-verbatim) " ")))
+        (t (error (format "Don't know how to parse: %s" el)))))
+
 ;;;###autoload
 (defun magit-org-todos-insert-org-todos ()
   "Insert org todos from the local todo.org."
@@ -70,11 +80,50 @@
               (when priority
                 (insert (propertize (concat "[#" (string priority) "]") 'face 'org-priority)  " "))
               (dolist (el title)
-                (if (and (listp el) (eq (car el) 'link))
-                    (insert (car (last el)))
-                  (insert el)))))
-          (insert ?\n))
+                (insert (magit-org-todos--handle-element el)))
+              (insert ?\n))))
         (insert ?\n)))))
+
+
+(defun magit-org-todos--handle-element (el)
+  "Handle org elements when rendering the TODO list in magit."
+  (cond
+   ((not (listp el)) el)
+   ((eq (car el) 'link)
+    (concat (car (last el)) " "))
+   ((eq (car el) 'verbatim)
+    (concat (propertize (plist-get (cadr el) :value) 'face 'org-verbatim) " "))
+   (t (error "Don't know how to parse this element: %s" el))))
+
+;;;###autoload
+(defun magit-org-todos-insert-org-todos ()
+  "Insert org todos from the local todo.org."
+  (when-let ((todo-file-path (magit-org-todos--todo-file-path))
+             (file-readable-p todo-file-path))
+    (let ((todos (with-temp-buffer
+                   (insert-file-contents todo-file-path)
+                   (org-mode)
+                   (org-element-map (org-element-parse-buffer) 'headline
+                     (lambda (headline)
+                       (when (eq (org-element-property :todo-type headline) 'todo)
+                         headline))))))
+      (magit-insert-section (org-todos-wrapper)
+        (magit-insert-heading "Todos:")
+        (dolist (todo todos)
+          (let ((keyword (org-element-property :todo-keyword todo))
+                (title (org-element-property :title todo))
+                (priority (org-element-property :priority todo))
+                (level (org-element-property :level todo)))
+            (magit-insert-section (org-todos title)
+              (insert (make-string level ?*))
+              (insert " " (propertize keyword 'face 'org-todo) " ")
+              (when priority
+                (insert (propertize (format "[#%c]" priority) 'face 'org-priority) " "))
+              (dolist (el title)
+                (insert (magit-org-todos--handle-element el)))
+              (insert ?\n))))
+        (insert ?\n)))))
+
 
 ;;;###autoload
 (defun magit-org-todos-autoinsert ()
